d32c6107b56c023e636f01d83fbf54bd73300e4c
ldelossa <louis.delos@gmail.com>
2022-08-08 22:34:40 -0400

    fix: make removal of mark not remove all marks
    Signed-off-by: ldelossa <louis.delos@gmail.com>

--- sway-mark.sh
+++ sway-mark.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+function clear_marks_current_window() {
+    mark=($(swaymsg -t get_tree| jq -r 'recurse(.nodes[]?) | recurse(.floating_nodes[]?) | select(.focused==true) | {marks} | .marks[0]'))
+    sway unmark $mark
+    exit
+}
+
 theme_overrides="listview { enabled: false;} num-rows { enabled: false;} num-filtered-rows { enabled: false;} case-indicator { enabled: false;} textbox-num-sep { enabled: false;}"
 mark=$(rofi -p "Set mark to (leave empty to remove)" -dmenu -theme-str "$theme_overrides")
-[[ -z $mark ]] && sway unmark && exit
-echo $mark
+[[ -z $mark ]] && clear_marks_current_window
 swaymsg focus tiling
 swaymsg mark $mark
